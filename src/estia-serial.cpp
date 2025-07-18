serek4/estia-serial

commit f9b441b913c0e4c5fd21727e890804033d2cdc38

frame begin changed to uint16, frame split update, sniffed frames dequeue limit

src/estia-serial.cpp
@@ -57,7 +57,7 @@ EstiaSerial::SnifferState EstiaSerial::sniffer() {
 		this->read(snifferBuffer);
 		if (this->splitSnifferBuffer()) {
 			for (auto& frame : sniffedFrames) {
-				if (frame.front() != FRAME_BEGIN) { continue; }
+				if (EstiaFrame::readUint16(frame, 0) != FRAME_BEGIN) { continue; }
 				if (decodeStatus(frame)) { continue; }
 				decodeAck(frame);
 			}
@@ -143,17 +143,17 @@ uint16_t EstiaSerial::getAck() {
 }
 
 bool EstiaSerial::splitSnifferBuffer() {
-	if (snifferBuffer.size() <= FRAME_MIN_LEN) { return false; }
+	if (snifferBuffer.size() <= FRAME_HEAD_LEN) { return false; }
 
 	uint8_t frameSize = 0;
 	while (!snifferBuffer.empty()) {
 		// calculate expected frame length from data
-		if (frameSize == 0 && sniffedFrame.size() > FRAME_DATA_LEN_OFFSET) {
+		if (frameSize == 0 && sniffedFrame.size() >= FRAME_HEAD_LEN
+		    && EstiaFrame::readUint16(sniffedFrame, 0) == FRAME_BEGIN) {
 			frameSize = sniffedFrame.at(FRAME_DATA_LEN_OFFSET) + FRAME_HEAD_AND_CRC_LEN;
 		}
 		// next frame begin detected in sniffer buffer
-		if (!sniffedFrame.empty() && snifferBuffer.size() >= 2 && snifferBuffer.front() == FRAME_BEGIN
-		    && snifferBuffer.at(1) == 0x00) {
+		if (!sniffedFrame.empty() && EstiaFrame::readUint16(snifferBuffer, 0) == FRAME_BEGIN) {
 			// frame shorter than expected and shorter than max
 			if (sniffedFrame.size() < frameSize && frameSize <= FRAME_MAX_LEN
 			    && sniffedFrame.size() <= FRAME_MAX_LEN) {
@@ -172,7 +172,7 @@ bool EstiaSerial::splitSnifferBuffer() {
 		if (frameSize != 0 && sniffedFrame.size() > frameSize) {
 			// check sniffed frame for next frame begin byte (skip first byte)
 			for (uint8_t idx = 1; idx < sniffedFrame.size(); idx++) {
-				if (sniffedFrame.at(idx) == FRAME_BEGIN) {
+				if (EstiaFrame::readUint16(sniffedFrame, idx) == FRAME_BEGIN) {
 					FrameBuffer firstFrame(sniffedFrame.begin(), sniffedFrame.begin() + idx);
 					sniffedFrame.erase(sniffedFrame.begin(), sniffedFrame.begin() + idx);
 					sniffedFrames.push_back(firstFrame);
@@ -185,6 +185,9 @@ bool EstiaSerial::splitSnifferBuffer() {
 		snifferBuffer.pop_front();
 	}
 	sniffedFrames.push_back(sniffedFrame);
+	while (sniffedFrames.size() >= SNIFFED_FRAMES_LIMIT) {
+		sniffedFrames.pop_front();
+	}
 	sniffedFrame.clear();
 
 	return true;
@@ -352,14 +355,12 @@ void EstiaSerial::write(const Frame& frame, bool disableRx) {
 
 void EstiaSerial::read(ReadBuffer& buffer, bool byteDelay) {
 	digitalWrite(LED_BUILTIN, LOW);
-	while (serial->available()) {    // read response
+	while (serial->available()) {
 		uint8_t newByte = serial->read();
 		if (byteDelay) { delay(ESTIA_SERIAL_BYTE_DELAY); }
 		buffer.push_back(newByte);
-		if (buffer.size() > 2) {
-			if (buffer.back() == 0x00 && buffer.at(buffer.size() - 2) == 0xa0) {    // new frame already began
-				break;
-			}
+		if (EstiaFrame::readUint16(buffer, buffer.size() - 2) == FRAME_BEGIN) {    // new frame already began
+			break;
 		}
 	}
 	digitalWrite(LED_BUILTIN, HIGH);

src/estia-serial.hpp
@@ -36,6 +36,8 @@ License along with this library; if not, see <https://www.gnu.org/licenses/>.
 #define ESTIA_SERIAL_BYTE_DELAY 5     // 4.2 ms minimum for baud 2400
 #define ESTIA_SERIAL_READ_DELAY 55    // minimum valid frame is 13 Bytes so min 54.6ms between frames
 
+#define SNIFFED_FRAMES_LIMIT 64
+
 #define REQUEST_RETRIES 2
 #define REQUEST_TIMEOUT 200
 

src/frames/frame.cpp
@@ -59,7 +59,7 @@ EstiaFrame::EstiaFrame(uint8_t type, uint8_t length)
 		length = FRAME_MIN_LEN;
 		buffer.resize(FRAME_MIN_LEN, 0x00);
 	}
-	buffer.front() = FRAME_BEGIN;
+	buffer.front() = FRAME_BEGIN >> 8;
 	buffer.at(FRAME_TYPE_OFFSET) = type;
 	buffer.at(FRAME_DATA_LEN_OFFSET) = dataLength;
 }

src/frames/frame.hpp
@@ -41,7 +41,7 @@ License along with this library; if not, see <https://www.gnu.org/licenses/>.
 #define FRAME_TYPE_UPDATE 0x1c
 #define FRAME_TYPE_STATUS 0x58
 
-#define FRAME_BEGIN 0xa0
+#define FRAME_BEGIN 0xa000
 
 #define FRAME_SRC_DST_MASTER 0x0800
 #define FRAME_SRC_DST_REMOTE 0x0040
@@ -73,8 +73,10 @@ License along with this library; if not, see <https://www.gnu.org/licenses/>.
 #define FRAME_UPDATE_DATA_LEN 0x0f
 #define FRAME_FORCE_DEFROST_DATA_LEN 0x0a
 
-#define FRAME_HEAD_AND_CRC_LEN 0x06
-#define FRAME_MIN_LEN FRAME_HEAD_AND_CRC_LEN + FRAME_MIN_DATA_LEN
+#define FRAME_HEAD_LEN 0x04
+#define FRAME_CRC_LEN 0x02
+#define FRAME_HEAD_AND_CRC_LEN (FRAME_HEAD_LEN + FRAME_CRC_LEN)
+#define FRAME_MIN_LEN (FRAME_HEAD_LEN + FRAME_MIN_DATA_LEN + FRAME_CRC_LEN)
 #define FRAME_MAX_LEN 45
 #define FRAME_HEARTBEAT_LEN 13
 #define FRAME_SET_MODE_LEN 17

commit 774f407f371e4a8b3a8c8d406ecc987a292231cf

fixed read() method splitting every frame at begin (bug introduced in f9b441b9)

src/estia-serial.cpp
@@ -359,7 +359,7 @@ void EstiaSerial::read(ReadBuffer& buffer, bool byteDelay) {
 		uint8_t newByte = serial->read();
 		if (byteDelay) { delay(ESTIA_SERIAL_BYTE_DELAY); }
 		buffer.push_back(newByte);
-		if (EstiaFrame::readUint16(buffer, buffer.size() - 2) == FRAME_BEGIN) {    // new frame already began
+		if (buffer.size() > 2 && EstiaFrame::readUint16(buffer, buffer.size() - 2) == FRAME_BEGIN) {    // new frame already began
 			break;
 		}
 	}